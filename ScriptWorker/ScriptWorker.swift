dbf2c1d40a0f6e8088a8c44b32f8256bd8603d35
Stephen Marquis <marquissm@gmail.com>
2016-08-20 16:24:06 -0700

    Add documentation comments and make non exiting action variants

--- ScriptWorker/ScriptWorker.swift
+++ ScriptWorker/ScriptWorker.swift
@@ -8,69 +8,68 @@
 
 import Foundation
 
-// Simple struct to encapsulate common scripting actions.
-struct ScriptWorker {
-    enum ScriptError : ErrorType {
-        case NotADirectory
-        case EmptyDirectoryStack
-    }
+/**
+ Struct that represents a file path to take actions on.
+ The represented path doesn't need to exist, but will throw errors if any actions are taken that require an existing path
 
-    // Current path of the worker
-    var path: String
+ All of the actions come in two variants. The standard method exits the program on any encountered errors, printing the received error.
 
-    // Name of the item, i.e., the last path component
-    var name: String {
-        return (path as NSString).lastPathComponent
-    }
+ Actions suffixed with '_safe' throw any encountered errors, in case you want to handle the error without program failure.
+*/
+public struct ScriptWorker {
 
-    // Helper var for operations that require a url
+    /// The file path represented by this struct
+    public var path: String
+
+    // private helper to get the url during file operations
     private var url: NSURL {
         return NSURL(fileURLWithPath: path)
     }
 
-    private var pathStack: [String] = []
-
     private let fileManager = NSFileManager.defaultManager()
 
-    // If no path is given, defaults to the processes current working directory
-    init(path: String? = nil) {
-        if let path = path {
-            self.path = path
-        } else {
-            self.path = fileManager.currentDirectoryPath
+
+    /// Name of the currently pointed to item, i.e., the last path component
+    public var name: String {
+        return (path as NSString).lastPathComponent
+    }
+
+    /// Initialize a ScriptWorker object with the given file path
+    public init(path: String) {
+        self.path = path
+    }
+
+    // MARK: Attributes
+
+    /// Indicates whether the current path is a file
+    public var isFile: Bool {
+        let status = fileStatus()
+        return status.exists && !status.directory
+    }
+
+    /// Indicates whether the current path is a directory
+    public var isDirectory: Bool {
+        let status = fileStatus()
+        return status.exists && status.directory
+    }
+    
+    /** 
+    Indicates whether the current path exists. Note it is better to attempt an action and handle failure than to check existence to predicate behavior. For example,
+
+        let _ = try script.remove_safe()
+
+    is better than
+
+        if script.exists {
+            script.remove()
         }
+    */
+    public var exists: Bool {
+        return fileStatus().exists
     }
 
-    // Change the workers current path by applying relativePath, and storing the previous path in the stack
-    mutating func pushPath(relativePath: String) {
-        let newURL = url.URLByAppendingPathComponent(relativePath).URLByStandardizingPath!
-        pathStack.append(path)
-        path = newURL.path!
-    }
-
-    // Change the current path back to the previous path. Must be matched with a previous call to push
-    mutating func popPath() {
-        guard let newPath = pathStack.last else {
-            exitMsg("Tried to pop directory with an empty directory stack!")
-        }
-        path = newPath
-    }
-
-    var isFile: Bool {
-        var isDirObj: ObjCBool = false
-        let exists = fileManager.fileExistsAtPath(path, isDirectory: &isDirObj)
-
-        return exists && !isDirObj.boolValue
-    }
-
-    var isDirectory: Bool {
-        var isDirObj: ObjCBool = false
-        let exists = fileManager.fileExistsAtPath(path, isDirectory: &isDirObj)
-
-        return exists && isDirObj.boolValue
-    }
-
-    var isSymlink: Bool {
+    /// Indicates whether the current path is a symbolic link
+    public var isSymlink: Bool {
         if let _ = try? fileManager.destinationOfSymbolicLinkAtPath(path) {
             return true
         }
@@ -78,52 +77,105 @@ struct ScriptWorker {
         return false
     }
 
-    func removeItem(force force: Bool = false) {
-        if force {
-            _ = try? fileManager.removeItemAtURL(url)
+    // Helper for figuring out attributes.
+    private func fileStatus() -> (exists: Bool, directory: Bool) {
+        var isDirObj: ObjCBool = false
+        let exists = fileManager.fileExistsAtPath(path, isDirectory: &isDirObj)
+        return (exists, isDirObj.boolValue)
+    }
+
+    // MARK: Actions
+
+    /// Remove the item pointed to by path. If 'force' is true, it ignores any errors
+    public func remove() {
+        exitOnError {
+            try remove_safe()
+        }
+    }
+
+    public func remove_safe() throws {
+        try fileManager.removeItemAtURL(url)
+    }
+
+    /// Create a directory at path.
+    public func makeDirectory(withIntermediates intermediates: Bool = false) {
+        exitOnError {
+            try makeDirectory_safe(withIntermediates: intermediates)
+        }
+    }
+
+    public func makeDirectory_safe(withIntermediates intermediates: Bool = false) throws {
+        try fileManager.createDirectoryAtURL(url, withIntermediateDirectories: intermediates, attributes: nil)
+    }
+
+    /// Copy the item at path to the location defined by 'toPath'. toPath can be absolute or relative
+    public func copy(to toPath: String) {
+        exitOnError {
+            try copy_safe(to: toPath)
+        }
+    }
+
+    public func copy_safe(to toPath: String) throws {
+        let destinationURL: NSURL
+        if (toPath as NSString).absolutePath {
+            destinationURL = NSURL(fileURLWithPath: toPath)
         } else {
-            exitOnError {
-                try fileManager.removeItemAtURL(url)
-            }
+            destinationURL = url.URLByAppendingPathComponent(toPath)
+        }
+        try fileManager.copyItemAtURL(url, toURL: destinationURL)
+    }
+
+    /// Create a symlink at path to the location defined by 'toPath'. toPath can be absolute or relative
+    public func symlink(to toPath: String) {
+        exitOnError {
+            try symlink_safe(to: toPath)
         }
     }
 
-    func makeDirectory(withIntermediates intermediates: Bool = false) {
-        exitOnError {
-            try fileManager.createDirectoryAtURL(url, withIntermediateDirectories: intermediates, attributes: nil)
+    public func symlink_safe(to toPath: String) throws {
+        if (toPath as NSString).absolutePath {
+            try fileManager.createSymbolicLinkAtURL(url, withDestinationURL: NSURL(fileURLWithPath: toPath))
+        } else {
+            // Use path based API to make sure it ends up relative
+            try fileManager.createSymbolicLinkAtPath(url.path!, withDestinationPath: toPath)
         }
     }
 
-    func copy(to toPath: String) {
-        exitOnError {
-            try fileManager.copyItemAtURL(url, toURL: NSURL(fileURLWithPath: toPath))
-        }
-    }
-
-    func symlink(to toPath: String) {
-        exitOnError {
-            if (toPath as NSString).absolutePath {
-                try fileManager.createSymbolicLinkAtURL(url, withDestinationURL: NSURL(fileURLWithPath: toPath))
-            } else {
-                // Use path based API to make sure it ends up relative
-                try fileManager.createSymbolicLinkAtPath(url.path!, withDestinationPath: toPath)
-            }
-        }
-    }
-
-    // Enumerates all items within self, generating a new ScriptWorker whos path is the full path of self (including 'name'), and `name` set to the item within the directory.
-    var contents: [ScriptWorker] {
-        var items: [String] = []
-        exitOnError {
-            items = try fileManager.contentsOfDirectoryAtPath(path)
+    /// Returns an array of ScriptWorker's representing the contents at 'path'. If path is not a directory, returns an empty array
+    public var contents: [ScriptWorker] {
+        guard let items = try? fileManager.contentsOfDirectoryAtPath(path) else {
+            return []
         }
 
         return items.map { item in
-            var subworker = ScriptWorker(path: path)
-            subworker.pushPath(item)
-            return subworker
+            let newPath = url.URLByAppendingPathComponent(item).path!
+            return ScriptWorker(path: newPath)
         }
     }
+
+    // MARK: Path stack
+
+    private var pathStack: [String] = []
+
+    // Change the workers current path, and storing the previous path in the stack. The passed in path can be absolute or relative
+    mutating func pushPath(newPath: String) {
+        pathStack.append(path)
+        if (newPath as NSString).absolutePath {
+            path = newPath
+        } else {
+            path = url.URLByAppendingPathComponent(newPath).URLByStandardizingPath!.path!
+        }
+    }
+
+    // Change the current path back to the previous path. Exits if the directory stack is empty
+    mutating func popPath() {
+        guard let newPath = pathStack.last else {
+            exitMsg("Tried to pop directory with an empty directory stack!")
+        }
+        path = newPath
+    }
+
+    // MARK: Tasks
 
     // Launch the task and return the status. Configure block for configuring stdout/stderr
     private func launchTask(command: String, arguments: [String] = [], configure: (NSTask -> Void)) -> Int {
