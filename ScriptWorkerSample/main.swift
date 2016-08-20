4bea1c31d9e37649917721e711c668b1644c0bcd
Stephen Marquis <marquissm@gmail.com>
2016-08-20 16:19:38 -0700

    Add sample target

---
+++ ScriptWorkerSample/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ScriptWorkerSample
+//
+//  Created by Steve Marquis on 8/20/16.
+//  Copyright © 2016 Stephen Marquis. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
