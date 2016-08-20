beebb73d20d458bba0fc753b4e80e5c4e42d34d0
Stephen Marquis <marquissm@gmail.com>
2016-08-20 14:20:00 -0700

    Add empty project

---
+++ ScriptWorker/ScriptWorker.h
@@ -0,0 +1,19 @@
+//
+//  ScriptWorker.h
+//  ScriptWorker
+//
+//  Created by Steve Marquis on 8/20/16.
+//  Copyright © 2016 Stephen Marquis. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ScriptWorker.
+FOUNDATION_EXPORT double ScriptWorkerVersionNumber;
+
+//! Project version string for ScriptWorker.
+FOUNDATION_EXPORT const unsigned char ScriptWorkerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScriptWorker/PublicHeader.h>
+
+
