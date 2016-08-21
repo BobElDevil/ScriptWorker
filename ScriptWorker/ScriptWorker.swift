41a8e8c81b0fe5bcf4898cb18987f058528f8979
Stephen Marquis <marquissm@gmail.com>
2016-08-20 18:08:44 -0700

    Create data operations and write the sample script

--- ScriptWorker/ScriptWorker.swift
+++ ScriptWorker/ScriptWorker.swift
@@ -153,6 +153,85 @@ public struct ScriptWorker {
         }
     }
 
+    // MARK: Data operations
+
+    /// Read the contents at path as an NSData
+    public func read() -> NSData {
+        var data: NSData! = nil
+        exitOnError {
+            data = try read_safe()
+        }
+        return data
+    }
+
+    public func read_safe() throws -> NSData {
+        let fileHandle = try NSFileHandle(forReadingFromURL: url)
+        return fileHandle.readDataToEndOfFile()
+    }
+
+    /// Read the contents at path as a utf-8 string
+    public func readString() -> String {
+        var string: String!
+        exitOnError {
+            string = try readString_safe()
+        }
+        return string
+    }
+
+    public func readString_safe() throws -> String {
+        let data = try read_safe()
+        let result = String(data: data, encoding: NSUTF8StringEncoding)
+        return result ?? ""
+    }
+
+    /// Read the contents at path as a utf-8 string, and return an array of strings separated by newlines
+    public func readLines() -> [String] {
+        var lines: [String]!
+        exitOnError {
+            lines = try readLines_safe()
+        }
+        return lines
+    }
+
+    public func readLines_safe() throws -> [String] {
+        return try readString_safe().componentsSeparatedByCharactersInSet(NSCharacterSet.newlineCharacterSet())
+    }
+
+    /// Write the given data to path
+    public func write(data: NSData) {
+        exitOnError {
+            try write_safe(data)
+        }
+    }
+
+    public func write_safe(data: NSData) throws {
+        try data.writeToURL(url, options: [])
+    }
+
+    /// Write the given string to path with the utf8 encoding
+    public func writeString(string: String) {
+        exitOnError {
+            try writeString_safe(string)
+        }
+    }
+
+    public func writeString_safe(string: String) throws {
+        let data = string.dataUsingEncoding(NSUTF8StringEncoding)!
+        try write_safe(data)
+    }
+
+    /// Write the array of lines to path separated by newlines with the utf8 encoding
+    public func writeLines(lines: [String]) {
+        exitOnError {
+            try writeLines_safe(lines)
+        }
+    }
+
+    public func writeLines_safe(lines: [String]) throws {
+        let string = lines.joinWithSeparator("\n")
+        try writeString_safe(string)
+    }
+
     // MARK: Path stack
 
     private var pathStack: [String] = []
@@ -169,10 +248,10 @@ public struct ScriptWorker {
 
     // Change the current path back to the previous path. Exits if the directory stack is empty
     public mutating func popPath() {
-        guard let newPath = pathStack.last else {
+        guard !pathStack.isEmpty else {
             exitMsg("Tried to pop directory with an empty directory stack!")
         }
-        path = newPath
+        path = pathStack.removeLast()
     }
 
     // MARK: Tasks
--- ScriptWorkerSample/main.swift
+++ ScriptWorkerSample/main.swift
@@ -10,7 +10,47 @@ import Foundation
 import ScriptWorker
 
 
-let script = ScriptWorker()
+// Proof of concept demo script that first creates a directory with 5 files in it, and a file listing a subset of those files to symlink.  It then reads in that list, and symlinks any entries found in DirOne into a new DirTwo
 
-print(script.path)
+var script = ScriptWorker()
 
+script.pushPath("Workspace")
+_ = try? script.remove_safe()
+script.makeDirectory()
+
+// Part one, create the files
+script.pushPath("DirOne")
+script.makeDirectory()
+
+for file in ["alpha", "beta", "charlie", "delta", "echo"] {
+    script.pushPath(file)
+    script.writeString("This file is \(file)")
+    script.popPath()
+}
+script.popPath()
+
+script.pushPath("linkList.txt")
+script.writeLines(["beta", "echo"])
+
+// Part two, create the symlinks
+let entries = script.readLines().filter{!$0.isEmpty}
+script.popPath()
+
+var readScript = script
+readScript.pushPath("DirOne")
+
+script.pushPath("DirTwo")
+script.makeDirectory()
+
+for entry in entries {
+    readScript.pushPath(entry)
+    defer { readScript.popPath() }
+
+    guard readScript.exists else {
+        continue
+    }
+
+    script.pushPath(entry)
+    script.symlink(to: readScript.path)
+    script.popPath()
+}
