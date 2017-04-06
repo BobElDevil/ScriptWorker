7c0d5c49b8b8dcf41291beb8b3df3f0ab47adbc2
Stephen Marquis <marquissm@gmail.com>
2017-04-06 12:10:45 -0700

    Write preprocessor and unit tests

--- swiftscript/Preprocessor.swift
+++ swiftscript/Preprocessor.swift
@@ -10,4 +10,92 @@ import Foundation
 
 // Simple container class so we can compile this into a unit test bundle
 class Preprocessor {
+    private enum LineType {
+        case script(String)
+        case framework(String)
+        case blank
+        case other
+
+        init(_ rawLine: String) {
+            let parse = { (line: String, prefix: String) -> String? in
+                let trimmedLine = line.trimmingCharacters(in: .whitespaces)
+                guard let range = trimmedLine.range(of: prefix, options: .anchored, range: nil, locale: nil) else {
+                    return nil
+                }
+
+                return trimmedLine.substring(from: range.upperBound).trimmingCharacters(in: .whitespaces)
+            }
+
+            let line = rawLine.trimmingCharacters(in: .whitespaces)
+            if let val = parse(line, "//!swiftscript") {
+                self = .script(val)
+            } else if let val = parse(line, "//!swiftsearch") {
+                self = .framework(val)
+            } else {
+                self = line.trimmingCharacters(in: .whitespaces).isEmpty ? .blank : .other
+            }
+        }
+    }
+
+    class func readLines(from filePath: String) -> [String] {
+        let scriptUrl = URL(fileURLWithPath: filePath)
+
+        let fileHandle = try! FileHandle(forReadingFrom: scriptUrl)
+        let data = fileHandle.readDataToEndOfFile()
+        guard let string = String(data: data, encoding: String.Encoding.utf8) else {
+            print("Failed to read content of \(scriptUrl)")
+            exit(1)
+        }
+
+
+        return string.components(separatedBy: .newlines)
+    }
+
+    class func filesAndFrameworks(for lines: [String], withDir dir: String) -> ([String], [String]) {
+        guard !lines.isEmpty else {
+            return ([], [])
+        }
+        var lines = lines
+        if lines[0].hasPrefix("#!") {
+            lines.removeFirst()
+        }
+
+        var filesToRead: [String] = []
+        var frameworkSearchPaths: [String] = []
+        var stop: Bool = false
+        while !lines.isEmpty && !stop {
+            let line = lines.removeFirst()
+            switch LineType(line) {
+            case let .script(file):
+                filesToRead.append(file)
+            case let .framework(path):
+                frameworkSearchPaths.append(path)
+            case .blank:
+                continue
+            case .other:
+                stop = true
+            }
+        }
+
+        return (resolve(paths: filesToRead, against: dir), resolve(paths: frameworkSearchPaths, against: dir))
+    }
+
+    private class func resolve(paths: [String], against dir: String) -> [String] {
+        return paths.map { ($0 as NSString).expandingTildeInPath }.map { path in
+            if (path as NSString).isAbsolutePath {
+                return path
+            } else {
+                return (dir as NSString).appendingPathComponent(path)
+            }
+        }
+    }
+
+    class func swiftArguments(for file: URL, additionalFiles: [String], searchPaths: [String]) -> [String] {
+        var args: [String] = []
+        args += searchPaths.flatMap { ["-F", $0] }
+        args.append(file.path)
+        args += additionalFiles
+        
+        return args
+    }
 }
--- swiftscriptTests/swiftscriptTests.swift
+++ swiftscriptTests/swiftscriptTests.swift
@@ -9,4 +9,47 @@
 import XCTest
 
 class swiftscriptTests: XCTestCase {
+
+    private func validate(files: [String], searchPaths: [String], inLines lines: [String], file: StaticString = #file, line: UInt = #line) {
+        let (scriptsAndDirs, foundSearchPaths) = Preprocessor.filesAndFrameworks(for: lines, withDir: "/Relative/Prefix")
+        XCTAssertEqual(scriptsAndDirs, files, file: file, line: line)
+        XCTAssertEqual(foundSearchPaths, searchPaths, file: file, line: line)
+    }
+    
+    func testNoScriptLines() {
+        validate(files: [], searchPaths: [], inLines: [])
+    }
+
+    func testScriptLinesWithoutWhitespace() {
+        validate(files: ["/file1", "/file2"], searchPaths: [], inLines: ["//!swiftscript /file1", "//!swiftscript /file2"])
+        validate(files: ["/file1", "/file2"], searchPaths: [], inLines: ["#!/usr/bin/stuff", "//!swiftscript /file1", "//!swiftscript /file2"])
+    }
+
+    func testScriptLinesWithWhitespace() {
+        validate(files: ["/file1", "/file2"], searchPaths: [], inLines: ["//!swiftscript /file1", "", "  ", "  ", "//!swiftscript /file2"])
+        validate(files: ["/file1", "/file2"], searchPaths: [], inLines: ["#!/usr/bin/stuff", "//!swiftscript /file1", "", "  ", "  ", "//!swiftscript /file2"])
+    }
+
+    func testScriptLinesStopParsing() {
+        validate(files: ["/file1"], searchPaths: [], inLines: ["//!swiftscript /file1", "class Stuff {", "//!swiftscript /file2"])
+        validate(files: ["/file1"], searchPaths: [], inLines: ["//!swiftscript /file1", "#!/shebang/not/on/first/line", "//!swiftscript /file2"])
+        validate(files: ["/file1"], searchPaths: [], inLines: ["#!/usr/bin/stuff", "//!swiftscript /file1", "#!/shebang/not/on/first/line", "//!swiftscript /file2"])
+        validate(files: ["/file1"], searchPaths: [], inLines: ["#!/usr/bin/stuff", "//!swiftscript /file1", "InterruptionAndWhitespace", "   ", "//!swiftscript /file2"])
+    }
+
+    func testScriptAndSearchPaths() {
+        validate(files: ["/file1"], searchPaths: ["/dir2"], inLines: ["//!swiftscript /file1", "//!swiftsearch /dir2"])
+        validate(files: ["/file1", "/file2"], searchPaths: ["/dir2"], inLines: ["//!swiftscript /file1", "//!swiftsearch /dir2", "//!swiftscript /file2"])
+    }
+
+    func testPathExpansions() {
+        validate(files: ["/Relative/Prefix/file1"], searchPaths: [], inLines: ["//!swiftscript file1"])
+        validate(files: ["\(FileManager.default.homeDirectoryForCurrentUser.path)/file1"], searchPaths: [], inLines: ["//!swiftscript ~/file1"])
+    }
+
+    func testCompilerArgGeneration() {
+        let args = Preprocessor.swiftArguments(for: URL(fileURLWithPath: "/my/script.swift"), additionalFiles: ["/my/localdep.swift"], searchPaths: ["/Library/Frameworks"])
+        XCTAssertEqual(args, ["-F", "/Library/Frameworks", "/my/script.swift", "/my/localdep.swift"])
+    }
+    
 }
