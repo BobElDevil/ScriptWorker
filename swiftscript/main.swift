8f7356747cb7052c4c9d75551ea0ec7f8b371a1d
Stephen Marquis <marquissm@gmail.com>
2017-04-07 11:58:50 -0700

    Write main file that performs the scripting

--- swiftscript/main.swift
+++ swiftscript/main.swift
@@ -23,3 +23,76 @@ import Foundation
  Any other arguments supplied to swiftscript will be provided to the resulting 'swift/swiftc' command used to compile everything together
 **/
 
+func runExec(task: String, args: [String], overrideExecutablePath: String? = nil) {
+    let passedInTask = overrideExecutablePath ?? task
+    let fullArgList = [passedInTask] + args
+    let cArgs = fullArgList.map { strdup($0) } + [nil]
+    task.withCString{ cTask in
+        let ret = execv(cTask, cArgs)
+        print("Failed to exec \(task) \(args)")
+        let errString = String(cString: strerror(errno))
+        print("Error \(ret) \(errString)")
+    }
+}
+
+// Returns the path to the executable in the temporary directory
+func setup(file: URL, withAdditionalFiles additionalFiles: [URL], searchPaths: [URL]) -> URL {
+    let workingDirectoryTemplate = NSTemporaryDirectory() + "/swiftscript-XXX"
+    guard let cstring = mkdtemp(UnsafeMutablePointer(mutating: workingDirectoryTemplate.cString(using: .utf8))) else {
+        print("Failed to create temporary directory for compilation: errno \(errno)")
+        exit(1)
+    }
+
+    let workingDirectory = URL(fileURLWithPath: String(cString: cstring))
+
+    let swiftFiles = Preprocessor.swiftURLs(for: additionalFiles)
+
+    let (mainFile, compiledFiles) = Preprocessor.setup(workingDirectory: workingDirectory, for: file, with: swiftFiles)
+
+    let process = Process()
+    process.launchPath = "/usr/bin/xcrun"
+
+    let executableURL = workingDirectory.appendingPathComponent(file.deletingPathExtension().lastPathComponent)
+
+    let args = ["swiftc", "-o", executableURL.path] + Preprocessor.swiftArguments(for: mainFile, additionalFiles: compiledFiles, searchPaths: searchPaths)
+    process.arguments = args
+    process.launch()
+    process.waitUntilExit()
+
+    return executableURL
+}
+
+guard CommandLine.arguments.count >= 2 else {
+    print("No script found to run!")
+    print("Usage: swiftscript <script-file> <options>")
+    exit(1)
+}
+
+let scriptURL = URL(fileURLWithPath: CommandLine.arguments[1])
+let remainingArgs = CommandLine.arguments.suffix(from: 2)
+
+let scriptDir = scriptURL.deletingLastPathComponent()
+
+let lines = Preprocessor.readLines(from: scriptURL)
+let (additionalFiles, searchPaths) = Preprocessor.filesAndFrameworks(for: lines, withDir: scriptDir)
+
+if additionalFiles.isEmpty {
+    // Just run swift directly instead of compiling the output since there's no additional files
+    let args = Preprocessor.swiftArguments(for: scriptURL, additionalFiles: additionalFiles, searchPaths: searchPaths)
+    runExec(task: "/usr/bin/xcrun", args: ["swift"] + args + remainingArgs)
+} else {
+    // Run swift compiler, then exec the resulting binary
+    let executable = setup(file: scriptURL, withAdditionalFiles: additionalFiles, searchPaths: searchPaths)
+
+    // Launch a separate process that cleans up our working directory once the process has already begun
+    let cleanupProcess = Process()
+    cleanupProcess.launchPath = "/bin/sh"
+    cleanupProcess.arguments = ["-c", "sleep 10; rm -r \(executable.deletingLastPathComponent().path)"]
+    cleanupProcess.launch()
+
+    let scriptDir = scriptURL.deletingLastPathComponent().path
+    scriptDir.withCString { cstr in
+        chdir(cstr)
+        runExec(task: executable.path, args: Array(remainingArgs), overrideExecutablePath: scriptURL.path)
+    }
+}
